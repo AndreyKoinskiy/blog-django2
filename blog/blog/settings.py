2738bf5de28d39313ca800522312234c169a0541
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2019-12-24 16:03:40 +0200

    change db on Postgres and adding FullTextSearch

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'blog_app.apps.BlogAppConfig',
     'django.contrib.sitemaps',
-    
+    'django.contrib.postgres',
+
     'taggit',
 ]
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,11 +81,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -123,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
 EMAIL_HOST = 'smtp.gmail.com'
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -4,7 +4,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.views.generic import ListView
 
-from .forms import EmailPostForm, CommentForm
+#postrges
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 
 from taggit.models import Tag
@@ -68,5 +71,18 @@ def post_detail(request, year, month, day, post):
     post_tag_ids = post.tags.values_list('id',flat =True)
     similar_posts = Post.published.filter(tags__in = post_tag_ids,).exclude(id = post.id)
     similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags','publish')[:4]
-    print(similar_posts)
     return render(request,'blog/post/detail.html',{'post': post,'new_comment':new_comment, 'comment_form':comment_form, 'comments':comments,'similar_posts': similar_posts})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        search_vector = SearchVector('title', 'body')
+        search_query = SearchQuery(query)
+        results = Post.objects.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search = query)
+    return render (request, 'blog/post/search.html',{'form':form, 'query':query, 'results':results})
